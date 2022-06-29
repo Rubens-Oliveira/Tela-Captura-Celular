245a2ff9a76bb46b5c2dacc17e807b6149df1ee0
Rubens <rubenscordeiromg@gmail.com>
2022-06-29 15:15:36 -0300

    Update Animação.js

--- Animação.js
+++ Animação.js
@@ -1,19 +1,19 @@
-const card = document.querySelector("#container"); //chamei meu seletor ou seja minha div container
+const card = document.querySelector("#container"); 
 
-card.addEventListener("mousemove", cardEffect); //mousemove e responsavel por localizar as coordenadas do mouse
+card.addEventListener("mousemove", cardEffect); 
 card.addEventListener("mouseleave", cardBack);
 card.addEventListener("mouseenter", cardEnter);
 
 function cardEffect(event) {
-  //criei a função do efeito do cartao (cardEffect)
-  const cardWidth = card.offsetWidth; //largura do cartao
-  const cardHeight = card.offsetHeight; //altura do cartao
-  const centerX = card.offsetLeft + cardWidth / 2; //pegar a distancia entre o elemento e o elemto pai, ou seja a distancia entre o cartao e borda e somar a largura do cartao
+  
+  const cardWidth = card.offsetWidth; 
+  const cardHeight = card.offsetHeight; 
+  const centerX = card.offsetLeft + cardWidth / 2; 
   const centerY = card.offsetTop + cardHeight / 2;
-  const positionX = event.clientX - centerX; //event.clientX vem juntocom o mousemove e conseguimos localizar o centro do cartao no eixo x
+  const positionX = event.clientX - centerX; 
   const positionY = event.clientY - centerY;
 
-  const rotateX = ((+1 * 25 * positionY) / (cardHeight / 2)).toFixed(2); //animação de rotação no eixo x
+  const rotateX = ((+1 * 25 * positionY) / (cardHeight / 2)).toFixed(2); 
   const rotateY = ((-1 * 25 * positionX) / (cardWidth / 2)).toFixed(2);
 
   card.style.transform = `perspective(1000px) rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
